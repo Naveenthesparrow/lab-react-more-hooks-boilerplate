83e88cb69872a1f2f5d9e5c5d420d853b401e32b
Naveenthesparrow <naveenneevan6380@gmail.com>
2024-01-04 12:06:14 +0530

    done

---
+++ morehooks/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Hook from "./components/hook";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Hook />
+    </>
+  );
+}
+
+export default App;
